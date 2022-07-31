3c0b2249fe027e821ee05e0ba4891b82de86900a
Hayden Bleasel <hello@haydenbleasel.com>
2022-07-31 18:51:58 -0400

    Add Fathom Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,40 +4,45 @@ import type { FC } from 'react';
 import { Toaster } from 'react-hot-toast';
 import { NextSeo } from 'next-seo';
 import Head from 'next/head';
+import useAnalytics from '../hooks/useAnalytics';
 
-const MyApp: FC<AppProps> = ({ Component, pageProps }) => (
-  <>
-    <Head>
-      <link rel="favicon" href="/favicon.png" type="image/png" />
-    </Head>
-    <NextSeo
-      title="San Andreas Radio"
-      description="A project by Hayden Bleasel"
-      canonical="https://sanandreasradio.com"
-      openGraph={{
-        type: 'website',
-        locale: 'en_US',
-        url: 'https://sanandreasradio.com',
-        title: 'San Andreas Radio',
-        description: 'A project by Hayden Bleasel',
-        images: [
-          {
-            url: 'https://sanandreasradio.com/cover.png',
-            width: 1200,
-            height: 630,
-            alt: 'San Andreas Radio',
-          },
-        ],
-      }}
-      twitter={{
-        handle: '@haydenbleasel',
-        site: '@haydenbleasel',
-        cardType: 'summary_large_image',
-      }}
-    />
-    <Component {...pageProps} />
-    <Toaster position="bottom-right" />
-  </>
-);
+const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
+  useAnalytics();
+
+  return (
+    <>
+      <Head>
+        <link rel="favicon" href="/favicon.png" type="image/png" />
+      </Head>
+      <NextSeo
+        title="San Andreas Radio"
+        description="A project by Hayden Bleasel"
+        canonical="https://sanandreasradio.com"
+        openGraph={{
+          type: 'website',
+          locale: 'en_US',
+          url: 'https://sanandreasradio.com',
+          title: 'San Andreas Radio',
+          description: 'A project by Hayden Bleasel',
+          images: [
+            {
+              url: 'https://sanandreasradio.com/cover.png',
+              width: 1200,
+              height: 630,
+              alt: 'San Andreas Radio',
+            },
+          ],
+        }}
+        twitter={{
+          handle: '@haydenbleasel',
+          site: '@haydenbleasel',
+          cardType: 'summary_large_image',
+        }}
+      />
+      <Component {...pageProps} />
+      <Toaster position="bottom-right" />
+    </>
+  );
+};
 
 export default MyApp;
