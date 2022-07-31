c4979ee37ce9f1e17e30abcad0164bea301d3688
Hayden Bleasel <hello@haydenbleasel.com>
2022-07-31 18:45:38 -0400

    Add metadata

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,39 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import type { FC } from 'react';
 import { Toaster } from 'react-hot-toast';
+import { NextSeo } from 'next-seo';
+import Head from 'next/head';
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }) => (
   <>
+    <Head>
+      <link rel="favicon" href="/favicon.png" type="image/png" />
+    </Head>
+    <NextSeo
+      title="San Andreas Radio"
+      description="A project by Hayden Bleasel"
+      canonical="https://sanandreasradio.com"
+      openGraph={{
+        type: 'website',
+        locale: 'en_US',
+        url: 'https://sanandreasradio.com',
+        title: 'San Andreas Radio',
+        description: 'A project by Hayden Bleasel',
+        images: [
+          {
+            url: 'https://sanandreasradio.com/cover.png',
+            width: 1200,
+            height: 630,
+            alt: 'San Andreas Radio',
+          },
+        ],
+      }}
+      twitter={{
+        handle: '@haydenbleasel',
+        site: '@haydenbleasel',
+        cardType: 'summary_large_image',
+      }}
+    />
     <Component {...pageProps} />
     <Toaster position="bottom-right" />
   </>
