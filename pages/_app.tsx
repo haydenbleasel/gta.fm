1405f4dbbecbc2328b985f9e4b32cd9d30bbc728
Hayden Bleasel <hello@haydenbleasel.com>
2022-10-29 21:06:45 -0400

    Replace Fathom with Vercel Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,45 +4,42 @@ import type { FC } from 'react';
 import { Toaster } from 'react-hot-toast';
 import { NextSeo } from 'next-seo';
 import Head from 'next/head';
-import useAnalytics from '../hooks/useAnalytics';
+import { Analytics } from '@vercel/analytics/react';
 
-const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
-  useAnalytics();
-
-  return (
-    <>
-      <Head>
-        <link rel="icon" href="/favicon.png" type="image/png" />
-      </Head>
-      <NextSeo
-        title="San Andreas Radio"
-        description="All you had to do was follow the damn train, CJ."
-        canonical="https://sanandreasradio.com"
-        openGraph={{
-          type: 'website',
-          locale: 'en_US',
-          url: 'https://sanandreasradio.com',
-          title: 'San Andreas Radio',
-          description: 'All you had to do was follow the damn train, CJ.',
-          images: [
-            {
-              url: 'https://sanandreasradio.com/cover.png',
-              width: 1200,
-              height: 630,
-              alt: 'San Andreas Radio',
-            },
-          ],
-        }}
-        twitter={{
-          handle: '@haydenbleasel',
-          site: '@haydenbleasel',
-          cardType: 'summary_large_image',
-        }}
-      />
-      <Component {...pageProps} />
-      <Toaster position="bottom-right" />
-    </>
-  );
-};
+const MyApp: FC<AppProps> = ({ Component, pageProps }) => (
+  <>
+    <Head>
+      <link rel="icon" href="/favicon.png" type="image/png" />
+    </Head>
+    <NextSeo
+      title="San Andreas Radio"
+      description="All you had to do was follow the damn train, CJ."
+      canonical="https://sanandreasradio.com"
+      openGraph={{
+        type: 'website',
+        locale: 'en_US',
+        url: 'https://sanandreasradio.com',
+        title: 'San Andreas Radio',
+        description: 'All you had to do was follow the damn train, CJ.',
+        images: [
+          {
+            url: 'https://sanandreasradio.com/cover.png',
+            width: 1200,
+            height: 630,
+            alt: 'San Andreas Radio',
+          },
+        ],
+      }}
+      twitter={{
+        handle: '@haydenbleasel',
+        site: '@haydenbleasel',
+        cardType: 'summary_large_image',
+      }}
+    />
+    <Component {...pageProps} />
+    <Toaster position="bottom-right" />
+    <Analytics />
+  </>
+);
 
 export default MyApp;
