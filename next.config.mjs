f4c2d7bbdb7ae4207c5ff33a7ad2bd528068c3d7
Hayden Bleasel <hello@haydenbleasel.com>
2023-12-19 20:52:55 -0500

    Finish updates

---
+++ next.config.mjs
@@ -0,0 +1,22 @@
+import { createSecureHeaders } from 'next-secure-headers';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: createSecureHeaders({
+          // HSTS Preload: https://hstspreload.org/
+          forceHTTPSRedirect: [
+            true,
+            { maxAge: 63072000, includeSubDomains: true, preload: true },
+          ],
+        }),
+      },
+    ];
+  },
+};
+
+export default nextConfig;
