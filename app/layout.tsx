02541f387de4cfb917c302002534d1157c976c63
Hayden Bleasel <hello@haydenbleasel.com>
2023-12-19 20:46:40 -0500

    Misc fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,12 @@ import type { FC, ReactNode } from 'react';
 import Analytics from '@/components/analytics';
 import Toaster from '@/components/toaster';
 import '@/styles/globals.css';
+import { createMetadata } from '@/lib/metadata';
+
+export const metadata: Metadata = createMetadata({
+  title: 'San Andreas Radio',
+  description: 'All you had to do was follow the damn train, CJ.',
+});
 
 const Layout: FC<{
   children: ReactNode;
