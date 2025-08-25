2052ef8f6912ca05d9ecb60612eb15f7b66ca705
Hayden Bleasel <hello@haydenbleasel.com>
2025-08-25 16:22:32 -0700

    Update background.tsx

--- components/background.tsx
+++ components/background.tsx
@@ -4,23 +4,29 @@ import dynamic from 'next/dynamic';
 
 const VIDEO_DURATION = 37_466;
 const ReactPlayer = dynamic(() => import('react-player'), { ssr: false });
-const random = Math.floor(Math.random() * VIDEO_DURATION);
-const src = `https://www.youtube.com/watch?v=ZvPem5XYBrU?t=${random}`;
 
-export const Background = () => (
-  <ReactPlayer
-    className="pointer-events-none fixed top-0 left-0 aspect-video min-h-screen w-screen select-none opacity-60"
-    config={{
-      youtube: {
-        disablekb: 1,
-      },
-    }}
-    controls={false}
-    height="100%"
-    loop
-    muted
-    playing
-    src={src}
-    width="100%"
-  />
-);
+export const Background = () => {
+  const random = Math.floor(Math.random() * VIDEO_DURATION);
+  const src = 'https://www.youtube.com/watch?v=ZvPem5XYBrU';
+
+  console.log(random);
+
+  return (
+    <ReactPlayer
+      className="pointer-events-none fixed top-0 left-0 aspect-video min-h-screen w-screen select-none opacity-60"
+      config={{
+        youtube: {
+          disablekb: 1,
+          start: random,
+        },
+      }}
+      controls={false}
+      height="100%"
+      loop
+      muted
+      playing
+      src={src}
+      width="100%"
+    />
+  );
+};
