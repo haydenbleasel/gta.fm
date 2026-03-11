05caad79104142bb99d2779ec8e789e6d162006a
Hayden Bleasel <hello@haydenbleasel.com>
2026-03-10 23:23:19 -0700

    Update index.ts

--- lib/stations/index.ts
+++ lib/stations/index.ts
@@ -24,7 +24,7 @@ export const stations: {
   {
     image: RadioX,
     name: "Radio X",
-    stream: "https://www.youtube.com/watch?v=rdrnAvPyza0",
+    stream: "https://www.youtube.com/watch?v=LKo2DoOb5nc",
   },
   {
     image: CSR,
